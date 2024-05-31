71fe4033269f1ad6d9154e7ccd4f2567b2514b53
michael-k <michaelsure9@gmail.com>
2024-05-31 08:35:46 +0300

    Refactoring GUI

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:async/async.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:task_management/blocs/task_events.dart';
 import 'package:task_management/blocs/task_state.dart';
@@ -32,4 +34,52 @@ void main() {
     expect(stateMachine.state(),  TaskState.state_(TaskStates.idle));
   });
 
+  test("CancelableOperation with future", () async {
+    var cancellableOperation = CancelableOperation.fromFuture(
+      Future.value('future result'),
+      onCancel: () => {debugPrint('onCancel')},
+    );
+
+    cancellableOperation.cancel(); // uncomment this to test cancellation
+
+    cancellableOperation.value.then((value) => {
+          debugPrint('then: $value'),
+        });
+    cancellableOperation.value.whenComplete(() => {
+          debugPrint('onDone'),
+        });
+  });
+
+  test("CancelableOperation with stream", () async {
+    var cancellableOperation = CancelableOperation.fromFuture(
+      Future.value('future result'),
+      onCancel: () => {debugPrint('onCancel')},
+    );
+
+    cancellableOperation.cancel();  // uncomment this to test cancellation
+
+    cancellableOperation.asStream().listen(
+          (value) => {debugPrint('value: $value')},
+          onDone: () => {debugPrint('onDone')},
+        );
+  });
+
+  test("CancelableCompleter is cancelled", () async {
+
+    CancelableCompleter completer = CancelableCompleter(onCancel: () {
+      debugPrint('onCancel');
+    });
+
+    completer.operation.cancel();  // uncomment this to test cancellation
+
+    completer.complete(Future.value('future result'));
+    debugPrint('isCanceled: ${completer.isCanceled}');
+    debugPrint('isCompleted: ${completer.isCompleted}');
+    completer.operation.value.then((value) => {
+      debugPrint('then: $value'),
+    });
+    completer.operation.value.whenComplete(() => {
+      debugPrint('onDone'),
+    });
+  });
 }
